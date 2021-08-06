d2b7b13c23ba31f522bf95a81e3d296e649de88c
gamestar <rinayumiho@gmail.com>
2021-08-06 14:58:55 +0800

    W8D5 done

---
+++ W8D5/canvas_practice/practice.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(){
+  let canvas = document.getElementById("mycanvas");
+  canvas.width = 500;
+  canvas.height = 500;
+  let ctx = canvas.getContext("2d");
+  ctx.fillStyle = "red";
+  ctx.fillRect(0, 0, 500, 500);
+  ctx.beginPath();
+  ctx.arc(100, 100, 20, 0, 2*Math.PI, true);
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 5;
+  ctx.stroke();
+  ctx.fillStyle = "yellow";
+  ctx.fill();
+});
